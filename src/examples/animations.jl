1908f14623267fc619bc93fb3e838231759ec4dc
eabogle <ebogle@pdx.edu>
2025-10-06 14:28:00 -0700

    use package necessary

--- src/examples/animations.jl
+++ src/examples/animations.jl
@@ -1,6 +1,6 @@
 # © Anna Weller, University of Cologne, 2023
 
-
+using MeGraPDE
 using Plots, Measures
 using Graphs, LinearAlgebra
 
@@ -13,6 +13,6 @@ function animate_diffusion(Γ::EquilateralMetricGraph, u0::Vector{Function}, T::
         u_Q=spectral_solution(Γ, σ, coef_t)
         plot_function_3d(Γ, u_Q, size=(150,150), lw=2, grid_off=true)
     end
-    gif(anim, fps=5)
+    gif(anim, joinpath(@__DIR__, "diffusion.gif"), fps=5)
+    println("Saved GIF to: ", joinpath(@__DIR__, "diffusion.gif"))
 end
-
